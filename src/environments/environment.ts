b7b97ed691002be93123cb651823392c59c8cf93
Matt <mkane126@gmail.com>
2025-05-18 20:53:32 -0400

    fixe sto deploy step

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,6 @@
 
 export const environment = {
   production: false,
-  url: 'https://api.the-odds-api.com/v4/sports/americanfootball_nfl/odds?apiKey=4ba1f8370b945e673c90e78efd2def89&regions=us&markets=h2h&dateFormat=iso&oddsFormat=american&bookmakers=draftkings&commenceTimeFrom=2025-09-03T00%3A00%3A00Z&commenceTimeTo=2026-01-09T00%3A00%3A00Z&includeLinks=true&includeSids=true&includeBetLimits=true'
 };
 
 /*
